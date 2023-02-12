a92b28c9e9b0e9bc2dcbf5c9e8465ced56472dea
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-02-12 01:39:14 -0600

    Proyecto en blanco
    La idea me llegó durante esta semana, del 7 al 10 de febrero del 2023.
    Va a ser un manual de consulta para el hackathon y el desarrollo de aplicaciones en general.

---
+++ Developer Manual/ViewController.swift
@@ -0,0 +1,18 @@
+/* ViewController.swift --> Developer Manual. Created by Miguel Torres on 12/02/23. */
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    
+
+}
+
