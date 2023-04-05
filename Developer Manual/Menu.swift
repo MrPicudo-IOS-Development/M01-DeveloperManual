8e5207f6f15bf3c1a317cd8ab6ce2c524c06f8f1
Miguel Torres <420093743@pcpuma.acatlan.unam.mx>
2023-04-05 14:22:07 -0600

    Primer referencia completa
    Se creó la referencia del Autolayout 1 que simula la distribución de los dados en la aplicación de Dicee.

---
+++ Developer Manual/Menu.swift
@@ -0,0 +1,18 @@
+/* Menu.swift --> Developer Manual. Created by Miguel Torres on 12/02/23. */
+
+import UIKit
+
+class Menu: UIViewController {
+
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+    
+
+}
+
